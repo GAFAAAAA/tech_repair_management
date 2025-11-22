58604df4fb5d1fca3a827bf98f184def2e0e6a93
Gareth Furlong <gareth.a.furlong@googlemail.com>
2025-11-22 10:01:04 +0000

    trying to fix models and variants

--- models/repair_model.py
+++ models/repair_model.py
@@ -7,4 +7,7 @@ class RepairModel(models.Model):
 
     name = fields.Char(string='Model', required=True)
     brand_id = fields.Many2one('tech.repair.brand', string='Brand')
+
+    category_id = fields.Many2one('tech.repair.category', string='Category')
+    variants = fields.one2many('tech.repair.variant', 'model_id', string='Variants')
     
